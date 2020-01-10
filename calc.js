112fcb4455af00558b54ee9d22cb80eb6f8279f7
jigali1 <59417315+jigali1@users.noreply.github.com>
2020-01-11 02:56:55 +0500

    Add files via upload

--- calc.js
+++ calc.js
@@ -5,23 +5,68 @@ screen.addEventListener('click', pressKey);
 let display = document.getElementById('screen');
 
 let key;
+let i = 0;
+let x = new Array();
+let z;
 
 function pressKeyAnim(event) {
-	
+	key = event.target;
+	let classN = key.className;
+	console.log(classN.includes('button'));
+	if (!classN.includes('button')) {return;}
 	if (event.type == 'mousedown') {
-		key = event.target;
+		
 		key.classList.add("button_pressed");
 	}
 	if (event.type == 'mouseup') {
 		key.classList.remove("button_pressed");
 	}
-	//key.style['border-top'] = '#000';
-	//key.style['border-left'] = '#fff';
-	//key.style['border-right'] = '#fff';
-	//key.style['border-bottom']
 }
+
 function pressKey(event) {
-	console.log(key.innerHTML)
+	let classN = key.className;
+	if (!classN.includes('button')) {return;}
 	let pKey = key.innerHTML;
-	display.innerHTML += pKey;
+	let w;
+	console.log(pKey==","); 
+	if (pKey.match(/[0-9]/) != null || pKey == ".") {
+		display.innerHTML += pKey;
+		return;
+	}
+	if (pKey == 'C') {
+		display.innerHTML = '';
+		i = 0;
+		return;
+	}
+	if (i == 0) {
+		x[i] = display.innerHTML;
+		i++;
+		z = pKey;
+		display.innerHTML += z;
+	} else {
+		x[1] = display.innerHTML.slice(x[0].length+1);
+	switch (z) {
+		case "+" :
+		w = +x[0] + +x[1];
+		display.innerHTML = w;
+		i = 0;
+		break;
+		case "-" :
+		w = +x[0] - +x[1];
+		display.innerHTML = w;
+		i = 0;
+		break;
+		case "/" :
+		w = +x[0] / +x[1];
+		display.innerHTML = w;
+		i = 0;
+		break;
+		case "*" :
+		w = +x[0] * +x[1];
+		display.innerHTML = w;
+		i = 0;
+		break;
+	}
+	}
+
 }
